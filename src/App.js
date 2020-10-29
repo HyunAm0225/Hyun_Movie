0558a482cfafb8ecd678499ffdbc946f225cfc26
HyunAm <tlfgjawlq@naver.com>
2020-10-29 13:09:26 +0900

    2.0 jsx와 react 연습해보기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Orange from "./Orange";
 function App() {
   return (
     <div>
       <h1>test</h1>
+      <Orange />
     </div>
   );
 }
---
+++ src/Orange.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Orange = () => {
+  return <h4>I Like Orange</h4>;
+};
+
+export default Orange;
