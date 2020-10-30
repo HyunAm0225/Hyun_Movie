055054be6872fa0e254420cab0c9317ec196b9af
HyunAm <tlfgjawlq@naver.com>
2020-10-31 01:06:38 +0900

    4.0 API를 가져오기 위한 axios세팅하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movie: [],
   };
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+  getMovies = async () => {
+    const movies = await axios.get();
+  };
+  async componentDidMount() {
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
