9ebec50a8b62b69983594540a2b73df44cbd99ae
HyunAm <tlfgjawlq@naver.com>
2020-10-31 01:30:26 +0900

    4.1 영화 내용들 rendering 하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -7,14 +8,39 @@ class App extends React.Component {
     movie: [],
   };
   getMovies = async () => {
-    const movies = await axios.get();
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+
+    this.setState({ movies, isLoading: false });
+
+    // console.log(movies);
   };
   async componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
